c1f9d2566f3cda0d21bd0b8fe01b2d9f896ceb13
ToprakArslann <toprakarslan0139@gmail.com>
2025-02-16 00:50:49 +0300

    added main code

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+using namespace cv;
+
+
+string ascii_chars = " .,:ilwW";
+char get_ascii_char(int brightness){
+    int index = (brightness * (ascii_chars.length() - 1) / 255);
+    return ascii_chars[index];
+}
+
+int main(){
+    VideoCapture cap("input.mp4");
+
+    if(!cap.isOpened()) {
+        cerr << "Cant open video!" << endl;
+        cin.get();
+        return -1;
+    }
+
+    Mat frame, gray_frame, resized_frame;
+
+    while (cap.read(frame)){
+        cvtColor(frame, gray_frame, COLOR_BGR2GRAY);
+        resize(gray_frame, resized_frame, Size(80,40));
+
+        string ascii_art;
+        for (int y = 0; y < resized_frame.rows; y++){
+            for (int x = 0; x < resized_frame.cols; x++){
+                int brightness = resized_frame.at<uchar>(y, x);
+                ascii_art += get_ascii_char(brightness); 
+            }
+            ascii_art += "\n";
+        }
+
+        cout << system("cls") << ascii_art;
+        this_thread::sleep_for(chrono::milliseconds(33));
+    }
+    return 0;
+}
+
