c44ea59c1b617a306af72a48e6ee38c5a6140000
ToprakArslann <toprakarslan0139@gmail.com>
2025-02-16 02:27:08 +0300

    added resize calculation.

--- src/main.cpp
+++ src/main.cpp
@@ -17,17 +17,24 @@ char get_ascii_char(int brightness){
 int main(){
     VideoCapture cap("input.mp4");
 
+    int widthFrame = cap.get(CAP_PROP_FRAME_WIDTH);
+    int heightFrame = cap.get(CAP_PROP_FRAME_HEIGHT);
+    int width = 250;
+    int height = 50;
+    height = (width * heightFrame / widthFrame) * 0.5;
+    width = (width * widthFrame / heightFrame) * 0.5;
+    
     if(!cap.isOpened()) {
         cerr << "Cant open video!" << endl;
         cin.get();
         return -1;
     }
-
+    
     Mat frame, gray_frame, resized_frame;
 
     while (cap.read(frame)){
         cvtColor(frame, gray_frame, COLOR_BGR2GRAY);
-        resize(gray_frame, resized_frame, Size(80,40));
+        resize(gray_frame, resized_frame, Size(width,height));
 
         string ascii_art;
         for (int y = 0; y < resized_frame.rows; y++){
